3356cbe4f6577f53dcb3be3a47b8048a61e79c23
ostrichick <gip4kun@gmail.com>
2022-07-16 21:47:19 +0900

    연습문제

--- workspace/Estudiar/src/j0713_practice/CarTest.java
+++ workspace/Estudiar/src/j0713_practice/CarTest.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 public interface CarTest {
   public static void main(String[] args) {
-    ArrayList<Car> carList = new ArrayList<Car>();
-    // carList.add(new Sonata());
-
+    ArrayList<Car2> carList = new ArrayList<Car2>();
+    carList.add(new Sonata());
+    carList.add(new Grandeur());
+    carList.add(new Avante());
+    carList.add(new Genesis());
+    for (Car2 car : carList) {
+      car.run();
+      System.out.println("=====");
+    }
+    //
     Bus bus = new Bus();
     AutoCar autoCar = new AutoCar();
 
@@ -18,10 +25,53 @@ public interface CarTest {
     autoCar.load();
     bus.stop();
     autoCar.stop();
+    System.out.println("==========");
+    Car2 aicar = new AICar();
+    aicar.run();
+    aicar.wiper();
+    Car2 mannualcar = new ManualCar();
+    mannualcar.run();
+    mannualcar.wiper();
   }
 }
 
-// class Sonata extends Car {}
+class AICar extends Car2 {
+
+  @Override
+  public void drive() {
+    System.out.println("자율 주행");
+    System.out.println("방향 전환");
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("차가 스스로 멈춥니다");
+  }
+
+  @Override
+  public void wiper() {
+    System.out.println("기상 환경에 따른 자동 와이퍼 조절");
+  }
+}
+
+class ManualCar extends Car2 {
+
+  @Override
+  public void drive() {
+    System.out.println("운전자가 운전");
+    System.out.println("핸들 조작 방향 전환");
+  }
+
+  @Override
+  public void stop() {
+    System.out.println("브레이크를 밟아 정지");
+  }
+
+  @Override
+  public void wiper() {
+    System.out.println("사람이 직접 와이퍼 조절");
+  }
+}
 
 class Bus extends Car {
 
