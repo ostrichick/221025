2d7013da7ad9558adfe9cf460dc1b9d647ced0a3
ostrichick <gip4kun@gmail.com>
2022-07-19 10:00:31 +0900

    c1

--- workspace/Estudiar/src/j0718/CarTest_Unfinished.java
+++ workspace/Estudiar/src/j0718/CarTest_Unfinished.java
@@ -1,38 +1,76 @@
 package j0718;
 
+import java.util.HashMap;
+
 class Car {
 
-  String name;
+	String name;
 
-  public Car() {}
+	public Car() {
+	}
 
-  public Car(String name) {
-    this.name = name;
-  }
+	public Car(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Car) {
+			Car temp = (Car) obj;
+			int tempNum = temp.hashCode();
+			int thisNum = hashCode();
+			if (tempNum == thisNum) {
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
 }
 
-public class CarTest {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Car other = (Car) obj;
+//		return Objects.equals(name, other.name);
+//		}
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (obj instanceof Student) {
+//			Student temp = (Student) obj;
+//			int tempNum = temp.hashCode();
+//			int thisNum = hashCode();
+//			if (tempNum == thisNum) {
+//				return true;
+//			}
+//			return false;
+//		}
+//		return false;
+//	}
+//	}
+//}
 
-  public static void main(String[] args) {
-    CarFactory factory = CarFactory.getInstance();
-    Car sonata1 = factory.createCar("연수 차");
-    Car sonata2 = factory.createCar("연수 차");
-    System.out.println(sonata1 == sonata2); // ture
+public class CarTest_Unfinished {
 
-    Car avante1 = factory.createCar("승연 차");
-    Car avante2 = factory.createCar("승연 차");
-    System.out.println(avante1 == avante2); // true
+	public static void main(String[] args) {
+		CarFactory factory = CarFactory.getInstance();
+		Car sonata1 = factory.createCar("연수 차");
+		Car sonata2 = factory.createCar("연수 차");
+		System.out.println(sonata1.hashCode());
+		System.out.println(sonata2.hashCode());
+		System.out.println(sonata1.equals(sonata2));
+		System.out.println(sonata1 == sonata2); // ture
 
-    System.out.println(sonata1 == avante1); // false
-  }
-}
-
-class CarFactory {
-
-  public void createCar(String name) {
-    Car name = new Car(name);
-  }
-}
+		Car avante1 = factory.createCar("승연 차");
+		Car avante2 = factory.createCar("승연 차");
 		System.out.println(avante1 == avante2); // true
 
 		System.out.println(sonata1 == avante1); // false
@@ -40,7 +78,20 @@ class CarFactory {
 }
 
 class CarFactory {
-	public void createCar(String name) {
-		Car name = new Car(name);
+	static CarFactory instance;
+	private HashMap<Car, String> hashMap;
+
+	CarFactory() {
+	}
+
+	public Car createCar(String name) {
+		return new Car("name");
+	}
+
+	public static CarFactory getInstance() {
+		if (instance == null) {
+			instance = new CarFactory();
+		}
+		return instance;
 	}
 }
