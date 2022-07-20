4300a4edd27898d6d4db22a1ca1003225321d9d1
ostrichick <gip4kun@gmail.com>
2022-07-20 12:27:06 +0900

    데이터베이스 수업 파일 이름 변경

--- Oracle/multiplier works.sql
+++ Oracle/multiplier works.sql
@@ -1,5 +1,6 @@
-﻿CREATE TABLE gugutbl (txt VARCHAR(100));
-DROP TABLE gugutbl;
+﻿DROP TABLE gugutbl;
+CREATE TABLE gugutbl (txt VARCHAR(100));
+
 
 CREATE OR REPLACE PROCEDURE whileProc AS
 	v_str VARCHAR(100);
@@ -11,7 +12,7 @@ BEGIN
 		v_str := '';
 		v_k := 1;
 			WHILE (v_k < 10) LOOP
-				v_str := v_str || ' ' || v_i || 'x' || v_k || '=' || v_i*v_k;
+				v_str := v_str || ' ' || v_i || 'x' || v_k || '=' || v_i * v_k;
 				v_k := v_k + 1;
 			END LOOP;
 		v_i := v_i + 1;
