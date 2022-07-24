bd1f05407da4aceed5280623cd773bccfea99133
ostrichick <gip4kun@gmail.com>
2022-07-24 20:46:54 +0900

    서블릿15강 한글깨짐 해결

--- workspace/JSPnew/src/main/java/com/newlecture/web/Nana.java
+++ workspace/JSPnew/src/main/java/com/newlecture/web/Nana.java
@@ -4,20 +4,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Nana2 extends HttpServlet {
-	/**
-	 * 
-	 */
-//	private static final long serialVersionUID = -5173251285006315519L;
-
+@WebServlet("/hello2")
+public class Nana extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		super.service(req, resp);
+		resp.setCharacterEncoding("UTF-8"); // 유니코드로 보냄
+		resp.setContentType("text/html; charset=UTF-8"); // 브라우저에게 유니코드로 해석하라고 명령
 		PrintWriter out = resp.getWriter();
-		out.println("야호~");
+		for (int i = 0; i < 100; i++) {
+			out.println((i + 1) + "ㅎㅇㅎㅇ날아ㅓㅎㅇㅎㅇㅎㅇㄴㄹ~~~~~~~<br>");
+		}
+
 	}
 }
