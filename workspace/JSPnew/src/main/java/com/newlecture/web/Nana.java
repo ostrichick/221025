32fbba4185b8e70ef63cb4f531b8049250b34af9
ostrichick <gip4kun@gmail.com>
2022-07-25 10:08:54 +0900

    0725아침

--- workspace/JSPnew/src/main/java/com/newlecture/web/Nana.java
+++ workspace/JSPnew/src/main/java/com/newlecture/web/Nana.java
@@ -2,22 +2,24 @@ package com.newlecture.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello")
 public class Nana extends HttpServlet {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5173251285006315519L;
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		super.service(req, resp);
-		PrintWriter out = resp.getWriter();
-		out.println("야호~");
-	}
+  /**
+   *
+   */
+  private static final long serialVersionUID = -5173251285006315519L;
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+    //		super.service(req, resp);
+    PrintWriter out = resp.getWriter();
+    out.println("야호~");
+  }
 }
